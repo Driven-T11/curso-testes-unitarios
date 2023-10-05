501251e129faa2e75bf772a2a1ba95389b059f18
leticia.chijo <leticia.chijo@gmail.com>
2023-10-05 20:27:07 -0300

    Prática testes unitários

--- aula05/tests/unit/pacientes.test.ts
+++ aula05/tests/unit/pacientes.test.ts
@@ -1,5 +1,26 @@
-describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+import { faker } from "@faker-js/faker";
+import { generateProtocolForPacient } from "../../src/protocols-generator";
+
+jest.mock("uuid", () => {
+  return {
+    v4: () => {
+      return "mock protocol"
+    }
+  }
+})
+
+describe("Protocol generation", () => {
+  it("Should create protocol", async () => {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const priority = faker.datatype.boolean()
+
+    const result = generateProtocolForPacient(firstName, lastName, priority)
+    expect(result).toEqual({
+      priority,
+      date: expect.any(Date),
+      pacient: `${firstName} ${lastName}`,
+      protocol: "mock protocol"
+    })
   });
 });
